9d03d7ba5211e7f885ce5d2871d0492a5351e27c
dianlujitao <dianlujitao@lineageos.org>
2018-02-01 22:51:56 +0800

    input: fingerprint: goodix: Honor proximity state
    Implement a poor man's pocket mode.
    
     * Receive proximity state from userspace
     * Disable IRQ while proximity state is near
    
    Based on fluxi's original idea (https://review.lineageos.org/#/c/152043/),
    adapted with goodix driver.
    
    Change-Id: Icb0561ea16bce28a64715b2eac3c9edd1fb90354

--- drivers/input/fingerprint/goodix/gf_spi.h
+++ drivers/input/fingerprint/goodix/gf_spi.h
@@ -137,6 +137,9 @@ struct gf_dev {
 	struct notifier_block notifier;
 	char device_available;
 	char fb_black;
+	char wait_finger_down;
+	struct work_struct work;
+	bool proximity_state; /* 0:far 1:near */
 };
 
 int gf_parse_dts(struct gf_dev* gf_dev);
